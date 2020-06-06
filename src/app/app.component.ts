1db4f246661b38bb81eed591be6b4e3a3dde47ac
vanak <ven2kaze@gmail.com>
2020-06-06 09:10:56 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,7 @@ export class AppComponent {
 
   constructor() {
     const firebaseConfig = {
-      apiKey: "AIzaSyAIdF6kXGH3En5XXCOs3s32bZQ-NCsdqIM",
-      authDomain: "trello-b2971.firebaseapp.com",
-      databaseURL: "https://trello-b2971.firebaseio.com",
-      projectId: "trello-b2971",
-      storageBucket: "trello-b2971.appspot.com",
-      messagingSenderId: "510234610894",
-      appId: "1:510234610894:web:2da273d7705981684bd49f"
+
     };
     firebase.initializeApp(firebaseConfig);
 
